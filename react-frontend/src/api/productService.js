cb0a40c9b8c6a98d49c32e09ad47d2434e0963a1
Bahaa Shklawon <bahaashklawon@gmail.com>
2025-08-20 09:58:42 +0200

    fix: update mock database entry for product with barcode '4752224003195'

--- react-frontend/src/api/productService.js
+++ react-frontend/src/api/productService.js
@@ -6,7 +6,7 @@ const mockDatabase = {
         price: '15.50 د.ل',
         imageUrl: 'https://images.unsplash.com/photo-1541167760496-1628856ab772?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1037&q=80',
     },
-    '9876543210987': {
+    '4752224003195': {
         id: 2,
         name: 'كتاب React للمبتدئين',
         price: '45.00 د.ل',
