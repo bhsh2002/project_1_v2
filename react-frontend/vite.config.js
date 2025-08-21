59ea9def90fb0225396a32a1e321f6b9aeee3110
Bahaa Shklawon <bahaashklawon@gmail.com>
2025-08-21 12:04:45 +0200

    feat: add https encrption

--- react-frontend/vite.config.js
+++ react-frontend/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0"
+    host: "0.0.0.0",
+    port: 5173,
+    https: {
+      key: fs.readFileSync('./192.168.188.245+1-key.pem'),
+      cert: fs.readFileSync('./192.168.188.245+1.pem'),
+    }
   }
 })
